8f1de3931c0c8cb8b37dfdf5a9f238da7eac256a
ozbeyahmet1 <ahmet1ozbey@gmail.com>
2024-11-02 03:18:04 +0300

    feat: removes unused elements from storybook homepage

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../ui/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
